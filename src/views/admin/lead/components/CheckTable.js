3070626b5ee469c2daef57c4c03da54eed2310db
naimatofficial <naimat.official.10@gmail.com>
2024-12-21 20:14:39 +0500

    fix advance search

--- src/views/admin/lead/components/CheckTable.js
+++ src/views/admin/lead/components/CheckTable.js
@@ -211,7 +211,7 @@ export default function CheckTable(props) {
 			const data = Object.fromEntries(
 				Object.entries(values).filter(([key, value]) => value !== "")
 			);
-			// fetchAdvancedSearch(data, pageIndex + 1, pageSize);
+			fetchAdvancedSearch(data, pageIndex + 1, pageSize);
 		} else {
 			fetchData(pageIndex + 1, pageSize);
 		}
@@ -261,7 +261,7 @@ export default function CheckTable(props) {
 				}
 			});
 
-			// fetchAdvancedSearch(data, 1, pageSize);
+			fetchAdvancedSearch(data, 1, pageSize);
 			setUpdatedPage(0);
 			setGopageValue(1);
 
