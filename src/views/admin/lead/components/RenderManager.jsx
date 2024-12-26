435317370d9a7197ccbf05b67cdf2b5cd6c655bc
hammadsarwar786 <hammadarain326@gmail.com>
2024-12-26 15:14:47 +0500

    agent backend set to null when manager changes

--- src/views/admin/lead/components/RenderManager.jsx
+++ src/views/admin/lead/components/RenderManager.jsx
@@ -25,7 +25,8 @@ const RenderManager = ({
 		const managerAssigned = e.target.value;
 		const dataObj = {
 			managerAssigned: managerAssigned || "",
-			agentAssigned: managerAssigned ? "" : undefined,
+			// agentAssigned: managerAssigned ? "" : undefined,
+			agentAssigned: "",
 		};
 
 		try {
@@ -87,9 +88,8 @@ const RenderManager = ({
 	) : (
 		<Text textStyle="sm">
 			{selectedManager
-				? `${
-						tree?.managers?.find((m) => m._id === selectedManager)?.firstName
-				  } ${tree?.managers?.find((m) => m._id === selectedManager)?.lastName}`
+				? `${tree?.managers?.find((m) => m._id === selectedManager)?.firstName
+				} ${tree?.managers?.find((m) => m._id === selectedManager)?.lastName}`
 				: "No Manager Assigned"}
 		</Text>
 	);
