450ed84446ad0e186057d2c5308aad08da8af3c6
Naimat Ullah <86405439+naimatofficial@users.noreply.github.com>
2025-04-23 20:11:48 +0500

    Revert "Expense and invoice apis "

--- src/views/admin/invoice/components/CheckTable.js
+++ src/views/admin/invoice/components/CheckTable.js
@@ -47,7 +47,6 @@ import { FaEdit, FaTrash } from 'react-icons/fa';
 import { buttonStyle } from 'utils/btn';
 import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
-import { useLocation } from "react-router-dom";
 
 export default function CheckTable(props) {
 	const {
@@ -106,9 +105,6 @@ export default function CheckTable(props) {
 		onClose: onAddClose,
 	} = useDisclosure();
 	const { isOpen, onOpen, onClose } = useDisclosure();
-	const location = useLocation();
-	const queryParams = new URLSearchParams(location.search);
-	const incomingPayment = queryParams.get("incomingPayment");
 
 	const columns = useMemo(() => dataColumn, [dataColumn]);
 	const data = useMemo(() => tableData, [tableData]);
@@ -126,12 +122,6 @@ export default function CheckTable(props) {
 	const handleClick = () => {
 		onAddOpen();
 	};
-
-	useEffect(() =>{
-		if(incomingPayment) {
-			onAddOpen();
-		}
-	},[])
 
 	const formik = useFormik({
 		initialValues,
