b13e9c456b3fcc144faac4ef5c49309fadebda39
naimatofficial <naimat.official.10@gmail.com>
2025-04-24 10:35:27 +0500

    update date filter

--- src/views/admin/leadPool-v2/components/DateFilterButton.js
+++ src/views/admin/leadPool-v2/components/DateFilterButton.js
@@ -3,7 +3,7 @@ import { RiCalendar2Line } from 'react-icons/ri';
 
 const DateFilterButton = ({ onClick }) => {
 	return (
-		<Tooltip label='Date Time Filtered' hasArrow>
+		<Tooltip label='Date Filtered' hasArrow>
 			<IconButton
 				icon={<RiCalendar2Line />}
 				aria-label='Filter Date'
