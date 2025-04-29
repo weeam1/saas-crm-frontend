05ec654ca3249ad1894aaa0645226df6a531f33d
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2025-04-29 09:50:18 +0500

    Invoice table tab changing

--- src/views/admin/invoice/index.js
+++ src/views/admin/invoice/index.js
@@ -1,32 +1,32 @@
-import BankAccounts from '../bankAccountsV2/index';
-import InvoiceDevelopers from '../developers/index';
-import TabNavigationDisplay from '../../../components/TabNavigationDisplay/TabNavigationDisplay';
+import BankAccounts from "../bankAccountsV2/index";
+import InvoiceDevelopers from "../developers/index";
+import TabNavigationDisplay from "../../../components/TabNavigationDisplay/TabNavigationDisplay";
 const InvoiceModule = () => {
-	const user = JSON.parse(localStorage.getItem('user'));
-	const role =
-		user?.role === 'superAdmin' ? 'superAdmin' : user?.roles[0]?.roleName;
+  const user = JSON.parse(localStorage.getItem("user"));
+  const role =
+    user?.role === "superAdmin" ? "superAdmin" : user?.roles[0]?.roleName;
 
-	const tabsData = [
-		{
-		  label: "Developers",
-		  title: "Developer Information",
-		  description:
-			"View essential details about developers including their names, contact emails, and city locations.",
-		  component: <BankAccounts />,
-		},
-		{
-		  label: "Bank Accounts",
-		  title: "Developer Bank Details",
-		  description:
-			"Access bank account information for developers including account names, numbers, IBANs, Swift codes, and associated bank details.",
-		  component: <InvoiceDevelopers />,
-		},
-	  ];
-	return (
-	<>
-	   <TabNavigationDisplay tabsData={tabsData} />
-	</>
-	);
+  const tabsData = [
+    {
+      label: "Bank Accounts",
+      title: "Developer Bank Details",
+      description:
+        "Access bank account information for developers including account names, numbers, IBANs, Swift codes, and associated bank details.",
+      component: <BankAccounts />,
+    },
+    {
+      label: "Developers",
+      title: "Developer Information",
+      description:
+        "View essential details about developers including their names, contact emails, and city locations.",
+      component: <InvoiceDevelopers />,
+    },
+  ];
+  return (
+    <>
+      <TabNavigationDisplay tabsData={tabsData} />
+    </>
+  );
 };
 
 export default InvoiceModule;
