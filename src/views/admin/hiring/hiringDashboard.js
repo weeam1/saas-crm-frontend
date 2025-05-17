4c9d8a7a40462901b3c50b7a98d703ff8f9abded
naimatofficial <naimat.official.10@gmail.com>
2025-05-17 15:36:02 +0500

    update the export attendance reports, hiring show to manager fix, interview points issues resloved

--- src/views/admin/hiring/hiringDashboard.js
+++ src/views/admin/hiring/hiringDashboard.js
@@ -12,124 +12,124 @@ import { useDispatch } from 'react-redux';
 import { addPositions } from '../../../redux/positionsSlice';
 import { IoSettings } from 'react-icons/io5';
 const HiringDashboard = () => {
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const user = JSON.parse(localStorage.getItem('user'));
+	const user = JSON.parse(localStorage.getItem('user'));
 
-    const isAdmin = user?.role === 'superAdmin';
-    const isManager = user?.roles[0]?.roleName === 'Manager';
+	const isAdmin = user?.role === 'superAdmin';
+	const isManager = user?.roles[0]?.roleName === 'Manager';
 
-    const { data, isLoading, refetch } = useFetchItemsQuery(
-        {
-            path: `/hiring/stats`,
-        },
-        { refetchOnMountOrArgChange: true }
-    );
-    const { data: positionOptions, isLoading: positionsLoading } =
-        useFetchItemsQuery(
-            {
-                path: `/positions/options`,
-            },
-            { refetchOnMountOrArgChange: true }
-        );
+	const { data, isLoading, refetch } = useFetchItemsQuery(
+		{
+			path: `/hiring/stats`,
+		},
+		{ refetchOnMountOrArgChange: true }
+	);
+	const { data: positionOptions, isLoading: positionsLoading } =
+		useFetchItemsQuery(
+			{
+				path: `/positions/options`,
+			},
+			{ refetchOnMountOrArgChange: true }
+		);
 
-    useEffect(() => {
-        if (!positionsLoading && positionOptions?.doc) {
-            dispatch(addPositions(positionOptions?.doc));
-        }
-    }, [positionsLoading, positionOptions, dispatch]);
+	useEffect(() => {
+		if (!positionsLoading && positionOptions?.doc) {
+			dispatch(addPositions(positionOptions?.doc));
+		}
+	}, [positionsLoading, positionOptions, dispatch]);
 
-    const { data: runningInterviews, isLoading: interviewLoading } =
-        useFetchItemsQuery(
-            {
-                path: `/interviews/running`,
-            },
-            { refetchOnMountOrArgChange: true }
-        );
+	const { data: runningInterviews, isLoading: interviewLoading } =
+		useFetchItemsQuery(
+			{
+				path: `/interviews/running`,
+			},
+			{ refetchOnMountOrArgChange: true }
+		);
 
-    const stats = [
-        {
-            title: 'Candidates',
-            total: data?.doc?.totalCandidates || 0,
-            icon: MdDashboard,
-            path: '/hiring/candidates',
-        },
-        {
-            title: 'Short Listed',
-            total: data?.doc?.totalShortListed || 0,
-            icon: FaUsers,
-            path: '/hiring/short-listed',
-        },
-        {
-            title: 'Interviewed Candidates',
-            total: data?.doc?.totalCompletedInterviews || 0,
-            icon: FaUserCheck,
-            path: '/hiring/interviewed-candidates',
-        },
-        // {
-        // 	title: 'Running Interviews',
-        // 	total: data?.doc?.totalRunningInterviews || 0,
-        // 	icon: FaUserCheck,
-        // 	path: '/hiring/running-interviews',
-        // },
-    ];
+	const stats = [
+		{
+			title: 'Candidates',
+			total: data?.doc?.totalCandidates || 0,
+			icon: MdDashboard,
+			path: '/hiring/candidates',
+		},
+		{
+			title: 'Short Listed',
+			total: data?.doc?.totalShortListed || 0,
+			icon: FaUsers,
+			path: '/hiring/short-listed',
+		},
+		{
+			title: 'Interviewed Candidates',
+			total: data?.doc?.totalCompletedInterviews || 0,
+			icon: FaUserCheck,
+			path: '/hiring/interviewed-candidates',
+		},
+		// {
+		// 	title: 'Running Interviews',
+		// 	total: data?.doc?.totalRunningInterviews || 0,
+		// 	icon: FaUserCheck,
+		// 	path: '/hiring/running-interviews',
+		// },
+	];
 
-    const filteredStats = isManager
-        ? stats.filter((stat) => stat.title === 'Short Listed')
-        : stats;
+	const filteredStats = isManager
+		? stats.filter((stat) => stat.title === 'Short Listed')
+		: stats;
 
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    return isLoading || interviewLoading ? (
-        <Loader />
-    ) : (
-        <Box>
-            <Flex justifyContent='flex-end' alignItems='center'>
-                {isAdmin && (
-                    <Button
-                        colorScheme='gray'
-                        borderRadius='5px'
-                        size={{ base: 'sm', md: 'md' }}
-                        px={{ base: 4, md: 6 }}
-                        py={{ base: 2, md: 3 }}
-                        fontSize={{ base: 'sm', md: 'md' }}
-                        leftIcon={<Icon as={IoSettings} boxSize={4} />}
-                        onClick={() => navigate('/hiring/settings')}
-                        mb={4}
-                    >
-                        Settings
-                    </Button>
-                )}
-            </Flex>
+	return isLoading || interviewLoading ? (
+		<Loader />
+	) : (
+		<Box>
+			<Flex justifyContent='flex-end' alignItems='center'>
+				{isAdmin && (
+					<Button
+						colorScheme='gray'
+						borderRadius='5px'
+						size={{ base: 'sm', md: 'md' }}
+						px={{ base: 4, md: 6 }}
+						py={{ base: 2, md: 3 }}
+						fontSize={{ base: 'sm', md: 'md' }}
+						leftIcon={<Icon as={IoSettings} boxSize={4} />}
+						onClick={() => navigate('/hiring/settings')}
+						mb={4}
+					>
+						Settings
+					</Button>
+				)}
+			</Flex>
 
-            <SimpleGrid columns={[1, 2, 3]} spacing={6} p={5}>
-                {filteredStats.map((stat, index) => (
-                    <MiniStatistics
-                        key={index}
-                        fontsize='md'
-                        // onClick={() => navigate(stat.path)}
-                        startContent={
-                            <IconBox
-                                w='56px'
-                                h='56px'
-                                bg='linear-gradient(90deg, #D99A36 0%,rgb(221, 184, 92) 100%)'
-                                icon={<Icon w='28px' h='28px' as={stat.icon} color='white' />} // Change icon here
-                            />
-                        }
-                        name={stat.title}
-                        value={stat.total || 0}
-                    />
-                ))}
-            </SimpleGrid>
+			<SimpleGrid columns={[1, 2, 3]} spacing={6} p={5}>
+				{filteredStats.map((stat, index) => (
+					<MiniStatistics
+						key={index}
+						fontsize='md'
+						// onClick={() => navigate(stat.path)}
+						startContent={
+							<IconBox
+								w='56px'
+								h='56px'
+								bg='linear-gradient(90deg, #D99A36 0%,rgb(221, 184, 92) 100%)'
+								icon={<Icon w='28px' h='28px' as={stat.icon} color='white' />} // Change icon here
+							/>
+						}
+						name={stat.title}
+						value={stat.total || 0}
+					/>
+				))}
+			</SimpleGrid>
 
-            {runningInterviews?.total > 0 && (
-                <RunningInterviews
-                    interviews={runningInterviews?.doc}
-                    totals={runningInterviews?.total}
-                />
-            )}
-        </Box>
-    );
+			{runningInterviews?.total > 0 && (
+				<RunningInterviews
+					interviews={runningInterviews?.doc}
+					totals={runningInterviews?.total}
+				/>
+			)}
+		</Box>
+	);
 };
 
 export default HiringDashboard;
--- src/views/admin/hiring/index.js
+++ src/views/admin/hiring/index.js
@@ -1,104 +1,114 @@
-import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import TabNavigationDisplay from "components/TabNavigationDisplay/TabNavigationDisplay";
-import HiringDashboard from "./hiringDashboard";
-import Candidates from "./candidates/index";
-import ShortListedCandidates from "./shortListedCandidates/index";
-import InterviewedCandidates from "./interviewedCandidates/index";
-import InterviewedRound from "./interviewedCandidates/Rounds";
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import TabNavigationDisplay from 'components/TabNavigationDisplay/TabNavigationDisplay';
+import HiringDashboard from './hiringDashboard';
+import Candidates from './candidates/index';
+import ShortListedCandidates from './shortListedCandidates/index';
+import InterviewedCandidates from './interviewedCandidates/index';
+import InterviewedRound from './interviewedCandidates/Rounds';
 
-const DEFAULT_TAB = "dashboard";
+const DEFAULT_TAB = 'dashboard';
 
 const Hiring = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const user = JSON.parse(localStorage.getItem("user"));
-  const isManager = user?.roles[0]?.roleName === "Manager";
-  const [tabKey, setTabKey] = useState(0);
+	const [searchParams, setSearchParams] = useSearchParams();
+	const user = JSON.parse(localStorage.getItem('user'));
+	const isManager = user?.roles[0]?.roleName === 'Manager';
+	const [tabKey, setTabKey] = useState(0);
 
-  const allTabs = [
-    {
-      label: "Dashboard",
-      param: "dashboard",
-      title: "Hiring Dashboard Overview",
-      description:
-        "Monitor key recruitment statistics, candidate pipelines, and ongoing interviews.",
-      component: <HiringDashboard key={tabKey} />,
-    },
-    {
-      label: "Candidates",
-      param: "candidates",
-      title: "All Registered Candidates",
-      description:
-        "View a list of all candidates who have applied. Filter, search, and manage candidate profiles here.",
-      component: <Candidates key={tabKey} />,
-    },
-    {
-      label: "Short Listed",
-      param: "short-listed",
-      title: "Short Listed Candidates",
-      description:
-        "Candidates who have been selected for the next round. Review and manage their progress.",
-      component: <ShortListedCandidates key={tabKey} />,
-    },
-    {
-      label: "Final Interviewed",
-      param: "final-interviewed",
-      title: "Final Interviewed Candidates Overview",
-      description:
-        "See candidates who have final round interviews. Track interview outcomes and feedback.",
-      component: <InterviewedRound key={tabKey} />,
-    },
-    {
-      label: "Interviewed Candidates",
-      param: "interviewed-candidates",
-      title: "Interviewed Candidates Overview",
-      description:
-        "See candidates who have completed their interviews. Track interview outcomes and feedback.",
-      component: <InterviewedCandidates key={tabKey} />,
-    },
-  ];
+	const allTabs = [
+		{
+			label: 'Dashboard',
+			param: 'dashboard',
+			title: 'Hiring Dashboard Overview',
+			description:
+				'Monitor key recruitment statistics, candidate pipelines, and ongoing interviews.',
+			component: <HiringDashboard key={tabKey} />,
+		},
+		{
+			label: 'Candidates',
+			param: 'candidates',
+			title: 'All Registered Candidates',
+			description:
+				'View a list of all candidates who have applied. Filter, search, and manage candidate profiles here.',
+			component: <Candidates key={tabKey} />,
+		},
+		{
+			label: 'Short Listed',
+			param: 'short-listed',
+			title: 'Short Listed Candidates',
+			description:
+				'Candidates who have been selected for the next round. Review and manage their progress.',
+			component: <ShortListedCandidates key={tabKey} />,
+		},
+		{
+			label: 'Final Interviewed',
+			param: 'final-interviewed',
+			title: 'Final Interviewed Candidates Overview',
+			description:
+				'See candidates who have final round interviews. Track interview outcomes and feedback.',
+			component: <InterviewedRound key={tabKey} />,
+		},
+		{
+			label: 'Interviewed Candidates',
+			param: 'interviewed-candidates',
+			title: 'Interviewed Candidates Overview',
+			description:
+				'See candidates who have completed their interviews. Track interview outcomes and feedback.',
+			component: <InterviewedCandidates key={tabKey} />,
+		},
+	];
 
-  const tabsData = isManager
-    ? allTabs.filter((tab) => tab.param === "short-listed")
-    : allTabs;
+	const tabsData = isManager
+		? allTabs.filter((tab) => tab.param === 'short-listed')
+		: allTabs;
 
-  const tabFromParams = searchParams.get("tab") || DEFAULT_TAB;
+	// const tabFromParams = searchParams.get("tab") || DEFAULT_TAB;
 
-  const activeTabIndex = Math.max(
-    0,
-    tabsData.findIndex((tab) => tab.param === tabFromParams.toLowerCase())
-  );
+	const defaultTab = tabsData[0]?.param || DEFAULT_TAB;
+	const tabFromParams = searchParams.get('tab') || defaultTab;
 
-  useEffect(() => {
-    if (
-      !searchParams.get("tab") ||
-      !tabsData.some((tab) => tab.param === searchParams.get("tab"))
-    ) {
-      setSearchParams({ tab: DEFAULT_TAB });
-    }
-  }, [searchParams, setSearchParams, tabsData]);
+	const activeTabIndex = Math.max(
+		0,
+		tabsData.findIndex((tab) => tab.param === tabFromParams.toLowerCase())
+	);
 
-  const handleTabChange = (index) => {
-    const tabParam = tabsData[index].param;
-    setSearchParams({ tab: tabParam });
-    if (index === activeTabIndex) {
-      setTabKey((prev) => prev + 1);
-    }
-  };
+	// useEffect(() => {
+	// 	if (
+	// 		!searchParams.get('tab') ||
+	// 		!tabsData.some((tab) => tab.param === searchParams.get('tab'))
+	// 	) {
+	// 		setSearchParams({ tab: DEFAULT_TAB });
+	// 	}
+	// }, [searchParams, setSearchParams, tabsData]);
 
-  return (
-    <>
-      <TabNavigationDisplay
-        tabsData={tabsData.map((tab) => ({
-          ...tab,
-          component:
-            tab.param === tabFromParams.toLowerCase() ? tab.component : null,
-        }))}
-        activeTab={activeTabIndex}
-        onTabChange={handleTabChange}
-      />
-    </>
-  );
+	useEffect(() => {
+		const tab = searchParams.get('tab');
+		if (!tab || !tabsData.some((t) => t.param === tab)) {
+			setSearchParams({ tab: defaultTab });
+		}
+	}, [searchParams, setSearchParams, tabsData]);
+
+	const handleTabChange = (index) => {
+		const tabParam = tabsData[index].param;
+		setSearchParams({ tab: tabParam });
+		if (index === activeTabIndex) {
+			setTabKey((prev) => prev + 1);
+		}
+	};
+
+	return (
+		<>
+			<TabNavigationDisplay
+				tabsData={tabsData.map((tab) => ({
+					...tab,
+					component:
+						tab.param === tabFromParams.toLowerCase() ? tab.component : null,
+				}))}
+				activeTab={activeTabIndex}
+				onTabChange={handleTabChange}
+			/>
+		</>
+	);
 };
 
 export default Hiring;
