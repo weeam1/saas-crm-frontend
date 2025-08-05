ecd5a11af60355b07812f205050a8e6f9cb9ce82
naimatofficial <naimat.official.10@gmail.com>
2025-08-05 18:03:03 +0500

    update lead menu

--- src/views/admin/lead/components/subComponents/LeadMenu.jsx
+++ src/views/admin/lead/components/subComponents/LeadMenu.jsx
@@ -187,7 +187,7 @@ const LeadMenu = ({
 					>
 						Open in WhatsApp
 					</MenuItem>
-					{user?.roles[0]?.roleName === 'Agent' && (
+					{/* {user?.roles[0]?.roleName === 'Agent' && (
 						<MenuItem
 							// onClick={() => {
 							// 	setTaskInits(lead);
@@ -197,7 +197,7 @@ const LeadMenu = ({
 						>
 							Create Follow Up
 						</MenuItem>
-					)}
+					)} */}
 
 					<MenuItem
 						onClick={() => {
