33ee8472bc8ca23c11cb4fef44abeb77aa2b1e88
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2025-08-21 13:48:11 +0500

    Survey filter aligment fix and change color of button clear

--- src/views/admin/survey/Component/FilterSearch.jsx
+++ src/views/admin/survey/Component/FilterSearch.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Flex, Box, Text, Stack, useDisclosure,Tooltip } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Text,
+  Stack,
+  useDisclosure,
+  Tooltip,
+} from "@chakra-ui/react";
 import TopPagination from "components/pagination/TopPagination";
 import CustomDatePicker from "components/datetime/CustomDatePicker";
 import DateFilterButton from "views/admin/lead-v2/components/DateFilterButton";
@@ -77,7 +84,11 @@ const FilterSearch = ({
         width="100%"
       >
         <DateFilterButton onClick={openModal} isForceOpen={forceTooltip} />
-        <ViewToggle view={view} handleView={handleViewChange} moduleView= "surveysView"/>
+        <ViewToggle
+          view={view}
+          handleView={handleViewChange}
+          moduleView="surveysView"
+        />
         {isModalOpen && (
           <DateFilter
             isOpen={isModalOpen}
@@ -138,7 +149,12 @@ const FilterSearch = ({
             </Box> */}
       </Flex>
 
-      <Flex justifyContent="space-between" my={2}>
+      <Flex
+        justifyContent="space-between"
+        my={2}
+        alignItems={"center"}
+        flexDir={{ base: "column", sm: "column", md: "row" }}
+      >
         {searchTags && <SearchTags searchTags={searchTags} />}
         {/* Clear Button */}
         {(endDate || startDate) && (
@@ -148,11 +164,12 @@ const FilterSearch = ({
               fontSize="sm"
               color="red.500"
               fontWeight="medium"
+              borderColor="red.500"
+              borderWidth="1px"
               px={3}
               py={1}
-              borderRadius="md"
+              borderRadius="full"
               _hover={{ bg: "red.50" }}
-              onClick={handleClear}
             >
               Clear
             </Text>
