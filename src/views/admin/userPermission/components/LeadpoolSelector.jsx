29456ef6fdf44103451a9000633f4f676e8b7570
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2025-08-21 12:29:19 +0500

    Modify the permission and lead pool sequence

--- src/views/admin/userPermission/components/LeadpoolSelector.jsx
+++ src/views/admin/userPermission/components/LeadpoolSelector.jsx
@@ -1,6 +1,5 @@
-import { Box, Text, Flex, Switch } from "@chakra-ui/react";
-import { Icon } from "@chakra-ui/react";
-import { MdOutlineAdminPanelSettings } from "react-icons/md";
+import { Box, Text, Flex, Switch, Icon, Radio, RadioGroup, Stack } from "@chakra-ui/react";
+import { MdOutlineAdminPanelSettings, MdOutlinePerson, MdOutlineGroup } from "react-icons/md";
 
 export default function LeadpoolSelector({ modules, setModules }) {
   const leadPoolModules = modules.filter((m) =>
@@ -24,14 +23,15 @@ export default function LeadpoolSelector({ modules, setModules }) {
 
   return (
     <Box
-      mb="4"
-      bg="gray.100"
+      mb="6"
+      bg="white"
       rounded="md"
-      p="4"
+      p="6"
       borderWidth="1px"
-      borderRadius="md"
+      borderColor="brand.300"
+      boxShadow="base"
     >
-      {/* <Flex align='center' justify='space-between' mb={4}>
+        {/* <Flex align='center' justify='space-between' mb={4}>
 				<Text color='gray.600'>Enable Leadpool modules</Text>
 				<Switch
 					colorScheme='green'
@@ -39,12 +39,18 @@ export default function LeadpoolSelector({ modules, setModules }) {
 					onChange={(e) => toggleLeadpool(e.target.checked)}
 				/>
 			</Flex> */}
-
-      <>
-        <Text color="gray.600" mb={4}>
-          Choose one Leadpool module (only one can be active at a time).
+      <Flex align="center" mb={4}>
+        <Icon as={MdOutlineGroup} w={6} h={6} color="brand.500" mr={2} />
+        <Text fontWeight="semibold" color="brand.600">
+          Leadpool Access
         </Text>
-        <Flex direction={{ base: "column", md: "row" }} gap="4" w="full">
+      </Flex>
+      
+      <Text color="gray.600" mb={4} fontSize="sm">
+        Select one leadpool access type (only one can be active at a time)
+      </Text>
+      
+      <Flex direction={{ base: "column", md: "column" }} gap="4" w="full">
           {leadPoolModules.map((module) => (
             <Box
               key={module.moduleId}
@@ -83,7 +89,6 @@ export default function LeadpoolSelector({ modules, setModules }) {
             </Box>
           ))}
         </Flex>
-      </>
     </Box>
   );
 }
--- src/views/admin/userPermission/components/PermissionCard.jsx
+++ src/views/admin/userPermission/components/PermissionCard.jsx
@@ -1,157 +1,157 @@
-import React from 'react';
+import React from "react";
 import {
-	Box,
-	Flex,
-	Text,
-	Switch,
-	Checkbox,
-	Divider,
-	SimpleGrid,
-} from '@chakra-ui/react';
-import PermissionIcon from './PermissionIcon';
+  Box,
+  Flex,
+  Text,
+  Switch,
+  Checkbox,
+  Divider,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import PermissionIcon from "./PermissionIcon";
 
 const PermissionCard = ({
-	module,
-	moduleIndex,
-	borderColor,
-	disabledBorderColor,
-	disabledTextColor,
-	handleModuleToggle,
-	handleSelectAll,
-	handleActionToggle,
+  module,
+  moduleIndex,
+  borderColor,
+  disabledBorderColor,
+  disabledTextColor,
+  handleModuleToggle,
+  handleSelectAll,
+  handleActionToggle,
 }) => {
-	const cleanModuleName = module.moduleName.replace(/^[\s,]+/, '').trim();
+  const cleanModuleName = module.moduleName.replace(/^[\s,]+/, "").trim();
 
-	return (
-		<Box
-			borderWidth='1px'
-			borderColor={borderColor}
-			borderRadius='md'
-			bg='gray.100'
-			w='full'
-			p={4}
-		>
-			{/* Module title and Check All */}
-			<Flex
-				justify='space-between'
-				align='center'
-				mb={2}
-				flexWrap='wrap'
-				gap={2}
-			>
-				<Box
-					fontWeight='bold'
-					fontSize={{ base: 'sm', md: 'md', lg: 'lg' }}
-					display='flex'
-					gap={2}
-					alignItems='center'
-					color='brand.600'
-				>
-					<PermissionIcon moduleName={cleanModuleName} />
+  return (
+    <Box
+      borderWidth="1px"
+      borderColor={borderColor}
+      borderRadius="md"
+      bg="white"
+      w="full"
+      p={4}
+	  boxShadow="base"
+    >
+      {/* Module title and Check All */}
+      <Flex
+        justify="space-between"
+        align="center"
+        mb={2}
+        flexWrap="wrap"
+        gap={2}
+      >
+        <Box
+          fontWeight="bold"
+          fontSize={{ base: "sm", md: "md", lg: "lg" }}
+          display="flex"
+          gap={2}
+          alignItems="center"
+          color="brand.600"
+        >
+          <PermissionIcon moduleName={cleanModuleName} />
 
-					<Text>{cleanModuleName}</Text>
-				</Box>
-				<Switch
-					colorScheme='green'
-					size='md'
-					isChecked={module.isModuleEnabled}
-					onChange={(e) => handleModuleToggle(moduleIndex, e.target.checked)}
-					_focus={{ boxShadow: 'none' }}
-					_active={{ boxShadow: 'none' }}
-				/>
-			</Flex>
+          <Text>{cleanModuleName}</Text>
+        </Box>
+        <Switch
+          colorScheme="brand"
+          size="md"
+          isChecked={module.isModuleEnabled}
+          onChange={(e) => handleModuleToggle(moduleIndex, e.target.checked)}
+          _focus={{ boxShadow: "none" }}
+          _active={{ boxShadow: "none" }}
+        />
+      </Flex>
+      {module?.actions.length > 0 && <Divider my={3} color={"brand.500"} />}
 
-			<Flex justify='flex-end' mb={2} mt={'-2px'}>
-				{module?.actions.length > 0 && (
-					<Checkbox
-						size='md'
-						colorScheme='green'
-						borderColor={
-							module.isModuleEnabled ? 'green.300' : disabledBorderColor
-						}
-						_focus={{ boxShadow: 'none' }}
-						_active={{ boxShadow: 'none' }}
-						_hover={{
-							borderColor: module.isModuleEnabled
-								? 'green.300'
-								: disabledBorderColor,
-						}}
-						isChecked={
-							module?.actions.length > 0
-								? module.actions.every((a) => a.isAllowed)
-								: false
-						}
-						onChange={(e) => handleSelectAll(moduleIndex, e.target.checked)}
-						isDisabled={!module.isModuleEnabled}
-						sx={{
-							'& .chakra-checkbox__control': {
-								borderColor: !module.isModuleEnabled
-									? disabledBorderColor
-									: 'gray.400',
-							},
-							'.chakra-checkbox__control': {
-								_focus: {
-									boxShadow: '0 0 0 2px',
-									borderColor: 'green.300',
-								},
-							},
-						}}
-					>
-						<Text
-							color={!module.isModuleEnabled ? disabledTextColor : 'inherit'}
-						>
-							Check All
-						</Text>
-					</Checkbox>
-				)}
-			</Flex>
+      <Flex justify="flex-end" mb={2} mt={"-2px"}>
+        {module?.actions.length > 0 && (
+          <Checkbox
+            size="md"
+            colorScheme="brand"
+            borderColor={
+              module.isModuleEnabled ? "brand.300" : disabledBorderColor
+            }
+            _focus={{ boxShadow: "none" }}
+            _active={{ boxShadow: "none" }}
+            _hover={{
+              borderColor: module.isModuleEnabled
+                ? "brand.300"
+                : disabledBorderColor,
+            }}
+            isChecked={
+              module?.actions.length > 0
+                ? module.actions.every((a) => a.isAllowed)
+                : false
+            }
+            onChange={(e) => handleSelectAll(moduleIndex, e.target.checked)}
+            isDisabled={!module.isModuleEnabled}
+            sx={{
+              "& .chakra-checkbox__control": {
+                borderColor: !module.isModuleEnabled
+                  ? disabledBorderColor
+                  : "gray.400",
+              },
+              ".chakra-checkbox__control": {
+                _focus: {
+                  boxShadow: "0 0 0 2px",
+                  borderColor: "brand.300",
+                },
+              },
+            }}
+          >
+            <Text
+              color={!module.isModuleEnabled ? disabledTextColor : "inherit"}
+            >
+              Check All
+            </Text>
+          </Checkbox>
+        )}
+      </Flex>
 
-			{module?.actions.length > 0 && <Divider my={3} />}
-
-			{/* Actions Grid */}
-			<SimpleGrid columns={{ base: 1, md: 2 }} spacing={2} flex='1'>
-				{module.actions.map((action, actionIndex) => (
-					<Checkbox
-						key={action.actionKey}
-						size='md'
-						colorScheme='green'
-						borderColor={
-							module.isModuleEnabled ? 'green.300' : disabledBorderColor
-						}
-						_focus={{ boxShadow: 'none' }}
-						_active={{ boxShadow: 'none' }}
-						_hover={{
-							borderColor: module.isModuleEnabled
-								? 'green.300'
-								: disabledBorderColor,
-						}}
-						isChecked={action.isAllowed}
-						onChange={() => handleActionToggle(moduleIndex, actionIndex)}
-						isDisabled={!module.isModuleEnabled}
-						sx={{
-							'& .chakra-checkbox__control': {
-								borderColor: !module.isModuleEnabled
-									? disabledBorderColor
-									: 'gray.400',
-							},
-							'.chakra-checkbox__control': {
-								_focus: {
-									boxShadow: '0 0 0 2px',
-									borderColor: 'green.300',
-								},
-							},
-						}}
-					>
-						<Text
-							color={!module.isModuleEnabled ? disabledTextColor : 'inherit'}
-						>
-							{action.name}
-						</Text>
-					</Checkbox>
-				))}
-			</SimpleGrid>
-		</Box>
-	);
+      {/* Actions Grid */}
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2} flex="1">
+        {module.actions.map((action, actionIndex) => (
+          <Checkbox
+            key={action.actionKey}
+            size="md"
+            colorScheme="brand"
+            borderColor={
+              module.isModuleEnabled ? "brand.300" : disabledBorderColor
+            }
+            _focus={{ boxShadow: "none" }}
+            _active={{ boxShadow: "none" }}
+            _hover={{
+              borderColor: module.isModuleEnabled
+                ? "brand.300"
+                : disabledBorderColor,
+            }}
+            isChecked={action.isAllowed}
+            onChange={() => handleActionToggle(moduleIndex, actionIndex)}
+            isDisabled={!module.isModuleEnabled}
+            sx={{
+              "& .chakra-checkbox__control": {
+                borderColor: !module.isModuleEnabled
+                  ? disabledBorderColor
+                  : "gray.400",
+              },
+              ".chakra-checkbox__control": {
+                _focus: {
+                  boxShadow: "0 0 0 2px",
+                  borderColor: "brand.300",
+                },
+              },
+            }}
+          >
+            <Text
+              color={!module.isModuleEnabled ? disabledTextColor : "inherit"}
+            >
+              {action.name}
+            </Text>
+          </Checkbox>
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
 };
 
 export default PermissionCard;
