30d415007eb03c7256398b8e714d914ad1420229
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2025-08-22 18:04:58 +0500

    Modify the sidebar icon center align on no collapse

--- src/components/sidebar/AppSidebar.jsx
+++ src/components/sidebar/AppSidebar.jsx
@@ -76,6 +76,9 @@ const SidebarItem = React.memo(function SidebarItem({
 			role='link'
 			data-testid={`sidebar-link-${route.moduleId}`}
 			transition='background 200ms ease'
+			display={"flex"}
+			flexDir={collapsed? "column" :"row"}
+			alignItems={collapsed?  "center" : "none" }
 		>
 			<Box
 				as='span'
