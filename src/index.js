042f72d071b03c13923c3d561498b25a7a435792
naimatofficial <naimat.official.10@gmail.com>
2025-10-06 19:02:24 +0500

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import 'assets/css/App.css';
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
-import 'assets/css/App.css';
 import {
 	BrowserRouter as Router,
 	Routes,
@@ -13,38 +10,7 @@ import {
 import AuthLayout from './layouts/auth';
 import AdminLayout from 'layouts/admin';
 import UserLayout from 'layouts/user';
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	useNavigate,
-} from 'react-router-dom';
-import AuthLayout from './layouts/auth';
-import AdminLayout from 'layouts/admin';
-import UserLayout from 'layouts/user';
 import {
-	ChakraProvider,
-	ColorModeScript,
-	Flex,
-	Box,
-	Spinner,
-} from '@chakra-ui/react';
-import theme from 'theme/theme';
-import { ThemeEditorProvider } from '@hypertheme-editor/chakra-ui';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Provider, useSelector } from 'react-redux';
-import store from './redux/store';
-import { useDispatch } from 'react-redux';
-import { getApi } from 'services/api';
-import { setActiveTree, setTree, setUsers } from './redux/localSlice';
-import ContextProvider from 'contexts/store';
-import LeadCycle from 'views/admin/leadCycle';
-import webSocketService from 'services/WebSocketService';
-import { newNotifyItem } from './redux/webSocketReducer';
-import { addAnnouncement } from './redux/announcementsSlice';
-import AnnouncementsModal from 'views/admin/announcement/components/AnnouncementsModal';
-import addNotification, { Notifications } from 'react-push-notification';
-import WeeamLoadingPage from './components/welcome/WeeamLoadingPage';
 	ChakraProvider,
 	ColorModeScript,
 	Flex,
@@ -69,15 +35,9 @@ import AnnouncementsModal from 'views/admin/announcement/components/Announcement
 import addNotification, { Notifications } from 'react-push-notification';
 import WeeamLoadingPage from './components/welcome/WeeamLoadingPage';
 
-import logo from 'assets/img/app-logo.jpeg';
 import logo from 'assets/img/app-logo.jpeg';
 
 // Import your audio file
-import newAnnouncementSound from 'assets/sounds/new-notification.mp3';
-import { requestNotificationPermission } from 'services/NotificationService';
-import Loader from 'components/loading/Loader';
-import useChunkErrorHandler from 'hooks/useChunkErrorHandler';
-import { getSmartTimezone } from 'hooks/useTimezone';
 import newAnnouncementSound from 'assets/sounds/new-notification.mp3';
 import { requestNotificationPermission } from 'services/NotificationService';
 import Loader from 'components/loading/Loader';
@@ -101,18 +61,9 @@ function App() {
 	useEffect(() => {
 		getSmartTimezone();
 	}, []);
-	useEffect(() => {
-		getSmartTimezone();
-	}, []);
 
 	const token = localStorage.getItem('token') || null;
-	const token = localStorage.getItem('token') || null;
 
-	const dispatch = useDispatch();
-	const [appLoaded, setAppLoaded] = useState(false);
-	const [splashScreen, setSplashScreen] = useState(true);
-	// const [permissionGranted, setPermissionGranted] = useState(false);
-	// const user = JSON.parse(localStorage.getItem('user'));
 	const dispatch = useDispatch();
 	const [appLoaded, setAppLoaded] = useState(false);
 	const [splashScreen, setSplashScreen] = useState(true);
@@ -121,22 +72,10 @@ function App() {
 
 	const { user } = useUserSession();
 	useNavigate();
-	const { user } = useUserSession();
-	useNavigate();
 
 	// initilize the web sockets
 	const { isConnected } = useSocketEvents();
-	// initilize the web sockets
-	const { isConnected } = useSocketEvents();
 
-	const showNotification = (customOptions) => {
-		const notificationOptions = {
-			theme: 'darkblue',
-			native: true,
-			duration: 20000,
-			icon: logo,
-			...customOptions,
-		};
 	const showNotification = (customOptions) => {
 		const notificationOptions = {
 			theme: 'darkblue',
@@ -149,21 +88,11 @@ function App() {
 		Notification.requestPermission();
 		addNotification(notificationOptions);
 	};
-		Notification.requestPermission();
-		addNotification(notificationOptions);
-	};
 
-	const user2 = useSelector((state) => state.user.user);
 	const user2 = useSelector((state) => state.user.user);
 
 	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [isModalOpen, setIsModalOpen] = useState(false);
 
-	// Splash screen
-	useEffect(() => {
-		const timer = setTimeout(() => setSplashScreen(false), 3600);
-		return () => clearTimeout(timer);
-	}, []);
 	// Splash screen
 	useEffect(() => {
 		const timer = setTimeout(() => setSplashScreen(false), 3600);
@@ -175,19 +104,9 @@ function App() {
 			const registerPayload = {
 				userId: user?._id,
 			};
-	useEffect(() => {
-		if (isConnected && user?._id) {
-			const registerPayload = {
-				userId: user?._id,
-			};
 
 			console.log('RIGSTER USER AGAIN RECONNECT');
-			console.log('RIGSTER USER AGAIN RECONNECT');
 
-			socketService.registerUser(registerPayload);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [user?._id, isConnected]);
 			socketService.registerUser(registerPayload);
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
@@ -195,28 +114,14 @@ function App() {
 
 	useEffect(() => {
 		if (!user?._id) return;
-	useEffect(() => {
-		if (!user?._id) return;
 
-		webSocketService.connect(user._id);
 		webSocketService.connect(user._id);
 
 		webSocketService.socket.onmessage = async (event) => {
 			try {
 				const socketData = JSON.parse(event.data);
 				console.log('WebSocket message:', socketData);
-		webSocketService.socket.onmessage = async (event) => {
-			try {
-				const socketData = JSON.parse(event.data);
-				console.log('WebSocket message:', socketData);
 
-				let notificationDetails = {};
-				const { type, data } = socketData;
-				const message = data?.message
-					? data?.message
-					: data?.lead_id
-						? `You have been assigned a new lead${data?.lead_name && `: ${data?.lead_name}`}`
-						: 'Check out the latest updates!';
 				let notificationDetails = {};
 				const { type, data } = socketData;
 				const message = data?.message
@@ -235,34 +140,11 @@ function App() {
 						dispatch(addAnnouncement(data));
 					}
 				}
-				// Handle announcements (type === 1)
-				if (type === 1) {
-					if (Array.isArray(data) && data.length > 0) {
-						data.forEach((announcement) =>
-							dispatch(addAnnouncement(announcement))
-						);
-					} else {
-						dispatch(addAnnouncement(data));
-					}
-				}
 
 				// Push notification if type is valid
 				if (type !== -1 && message) {
 					dispatch(newNotifyItem(socketData));
-				// Push notification if type is valid
-				if (type !== -1 && message) {
-					dispatch(newNotifyItem(socketData));
 
-					notificationDetails = {
-						title:
-							type === 1
-								? 'New Announcement'
-								: type === 2
-									? 'Interview Invite'
-									: 'New Notification',
-						message,
-					};
-				}
 					notificationDetails = {
 						title:
 							type === 1
@@ -285,22 +167,7 @@ function App() {
 				// else {
 				// 	toast.success('Check out the latest updates!');
 				// }
-				// Request and send notifications
-				const isGranted = await requestNotificationPermission();
-				if (isGranted) {
-					showNotification({
-						title: notificationDetails.title,
-						message: notificationDetails.message,
-					});
-				}
-				// else {
-				// 	toast.success('Check out the latest updates!');
-				// }
 
-				// Play notification sound
-				await announcementSound
-					.play()
-					.catch((error) => console.error('Error playing sound:', error));
 				// Play notification sound
 				await announcementSound
 					.play()
@@ -312,17 +179,7 @@ function App() {
 				console.error('Error handling WebSocket message:', error);
 			}
 		};
-				// Open the modal and clear previous notification
-				setIsModalOpen(true);
-			} catch (error) {
-				console.error('Error handling WebSocket message:', error);
-			}
-		};
 
-		return () => {
-			webSocketService.socket.onmessage = null;
-		};
-	}, [dispatch, user]);
 		return () => {
 			webSocketService.socket.onmessage = null;
 		};
@@ -331,26 +188,14 @@ function App() {
 	const getToken = () => {
 		return localStorage.getItem('token') || null;
 	};
-	const getToken = () => {
-		return localStorage.getItem('token') || null;
-	};
 
-	const fetchTree = async () => {
-		setAppLoaded(false);
-		const response = await getApi('api/user/tree');
-		const data = response.data || null;
 	const fetchTree = async () => {
 		setAppLoaded(false);
 		const response = await getApi('api/user/tree');
 		const data = response.data || null;
 
 		dispatch(setTree(data));
-		dispatch(setTree(data));
 
-		setTimeout(() => {
-			setAppLoaded(true);
-		}, 0);
-	};
 		setTimeout(() => {
 			setAppLoaded(true);
 		}, 0);
@@ -360,18 +205,9 @@ function App() {
 		setAppLoaded(false);
 		const response = await getApi('api/v2/user/active_tree');
 		const data = response.data || null;
-	const fetchActiveTree = async () => {
-		setAppLoaded(false);
-		const response = await getApi('api/v2/user/active_tree');
-		const data = response.data || null;
 
 		dispatch(setActiveTree(data));
-		dispatch(setActiveTree(data));
 
-		setTimeout(() => {
-			setAppLoaded(true);
-		}, 0);
-	};
 		setTimeout(() => {
 			setAppLoaded(true);
 		}, 0);
@@ -382,16 +218,7 @@ function App() {
 		const response = await getApi('api/user/');
 		const data = response.data || null;
 		dispatch(setUsers(data?.user));
-	const fetchUsers = async () => {
-		setAppLoaded(false);
-		const response = await getApi('api/user/');
-		const data = response.data || null;
-		dispatch(setUsers(data?.user));
 
-		setTimeout(() => {
-			setAppLoaded(true);
-		}, 0);
-	};
 		setTimeout(() => {
 			setAppLoaded(true);
 		}, 0);
@@ -406,52 +233,12 @@ function App() {
 			setAppLoaded(true);
 		}
 	}, [user2]);
-	useEffect(() => {
-		if (getToken() && user2) {
-			fetchTree();
-			fetchActiveTree();
-			fetchUsers();
-		} else if (!getToken()) {
-			setAppLoaded(true);
-		}
-	}, [user2]);
 
 	// Show splash screen
 	if (!appLoaded || splashScreen) {
 		return <WeeamLoadingPage />;
 	}
-	// Show splash screen
-	if (!appLoaded || splashScreen) {
-		return <WeeamLoadingPage />;
-	}
 
-	return (
-		<>
-			<Notifications />
-			{isModalOpen && (
-				<AnnouncementsModal
-					isOpen={isModalOpen}
-					onClose={() => setIsModalOpen(false)}
-				/>
-			)}
-			<ToastContainer />
-			<Routes>
-				{token && user?.role ? (
-					<Route path='/*' element={<AdminLayout />} />
-				) : (
-					// user?.role === 'user' ? (
-					// 	<Route path='/*' element={<UserLayout />} />
-					// ) : user?.role === 'superAdmin' ? (
-					// 	<Route path='/*' element={<AdminLayout />} />
-					// ) : (
-					// 	''
-					// )
-					<Route path='/*' element={<AuthLayout />} />
-				)}
-			</Routes>
-			{/* <LeadCycle /> */}
-		</>
-	);
 	return (
 		<>
 			<Notifications />
@@ -482,23 +269,6 @@ function App() {
 }
 
 ReactDOM.render(
-	<Provider store={store}>
-		<ContextProvider>
-			<ChakraProvider theme={theme} cssVarsRoot='body'>
-				<React.StrictMode>
-					<ThemeEditorProvider>
-						<Router>
-							<ColorModeScript
-								initialColorMode={theme.config.initialColorMode}
-							/>
-							<App />
-						</Router>
-					</ThemeEditorProvider>
-				</React.StrictMode>
-			</ChakraProvider>
-		</ContextProvider>
-	</Provider>,
-	document.getElementById('root')
 	<Provider store={store}>
 		<ContextProvider>
 			<ChakraProvider theme={theme} cssVarsRoot='body'>
