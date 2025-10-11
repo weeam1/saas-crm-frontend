baaba7bf0f1dbb8b4fb5d99066ef011f77be5f7a
naimatofficial <naimat.official.10@gmail.com>
2025-10-11 11:34:03 +0500

    update colors

--- src/views/admin/whatsapp-v2/utils/getAvatarColor.js
+++ src/views/admin/whatsapp-v2/utils/getAvatarColor.js
@@ -3,13 +3,27 @@ export const getAvatarColor = (input) => {
 		'#E57373', // red
 		'#F06292', // pink
 		'#BA68C8', // purple
+		'#9575CD', // deep purple
+		'#7986CB', // indigo
 		'#64B5F6', // blue
+		'#4FC3F7', // light blue
+		'#4DD0E1', // cyan
 		'#4DB6AC', // teal
 		'#81C784', // green
+		'#AED581', // light green
+		'#DCE775', // lime
 		'#FFD54F', // amber
 		'#FFB74D', // orange
+		'#FF8A65', // deep orange
 		'#A1887F', // brown
 		'#90A4AE', // blue gray
+		'#BDBDBD', // gray
+		'#F48FB1', // light pink
+		'#CE93D8', // soft violet
+		'#80CBC4', // seafoam
+		'#81D4FA', // sky blue
+		'#AED581', // apple green
+		'#FFCC80', // peach
 	];
 
 	let hash = 0;
