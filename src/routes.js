12ce3435c8aa8faaea80df1e175a30f8fd057a20
naimatofficial <naimat.official.10@gmail.com>
2025-10-23 16:37:35 +0500

    update routes

--- src/routes.js
+++ src/routes.js
@@ -110,6 +110,9 @@ const AllListing = React.lazy(
 const MyListing = React.lazy(
 	() => import('views/admin/Listing/Component/MyListing')
 );
+const ViewRequestListing = React.lazy(
+	() => import('views/admin/Listing/Component/ViewRequest')
+);
 const PendingListing = React.lazy(
 	() => import('views/admin/Listing/Component/PendingListings')
 );
@@ -655,7 +658,43 @@ const routes = [
 		parentName: 'Listing',
 		component: RejectedViewRequest,
 	},
+	// View Request Listing routes ----------
+	{
+		moduleId: 'view_request_listing',
+		name: 'View Request Listing',
+		layout: [ROLE_PATH.superAdmin, ROLE_PATH.user],
+		path: '/view-request-listing',
+		icon: <Icon as={FaList} width='20px' height='20px' color='inherit' />,
+		component: ViewRequestListing,
+	},
 
+	{
+		moduleId: 'view_request_listing',
+		name: 'Pending View Request',
+		layout: [ROLE_PATH.superAdmin, ROLE_PATH.user],
+		path: '/view-request-listing/pending-view-listing',
+		under: 'view_request_listing',
+		parentName: 'Listing',
+		component: PendingViewRequest,
+	},
+	{
+		moduleId: 'view_request_listing',
+		name: 'Approved View Request',
+		layout: [ROLE_PATH.superAdmin, ROLE_PATH.user],
+		path: '/view-request-listing/approved-view-listing',
+		under: 'view_request_listing',
+		parentName: 'Listing',
+		component: ApprovedViewRequest,
+	},
+	{
+		moduleId: 'listing',
+		name: 'Rejected View Request',
+		layout: [ROLE_PATH.superAdmin, ROLE_PATH.user],
+		path: '/view-request-listing/reject-view-listing',
+		under: 'view_request_listing',
+		parentName: 'Listing',
+		component: RejectedViewRequest,
+	},
 	// Survey Routes
 
 	{
