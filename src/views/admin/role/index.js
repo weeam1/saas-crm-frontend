4b33850456fdb1936779fa506389d60a2abffe02
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2025-10-27 12:30:22 +0500

    Role Table modfied with api integration and add modal added

--- src/views/admin/role/index.js
+++ src/views/admin/role/index.js
@@ -1,72 +1,226 @@
-import { AddIcon } from '@chakra-ui/icons';
-import { Button, Grid, GridItem, useDisclosure } from '@chakra-ui/react';
-import CheckTable from './components/CheckTable';
-import { useEffect, useState } from 'react';
-import { getApi } from 'services/api';
-import AppButton from 'components/shared/AppButton';
-import { IoArrowBack } from 'react-icons/io5';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Box,
+  Flex,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from "@chakra-ui/react";
+import AppButton from "components/shared/AppButton";
+import { IoArrowBack } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+import { useFetchItemsQuery } from "api/apiSlice";
+import TopPagination from "components/pagination/TopPagination";
+import CountUpComponent from "components/countUpComponent/countUpComponent";
+import { AddIcon } from "@chakra-ui/icons";
+import TableLoading from "components/loading/TableLoading";
+import NoData from "views/admin/lead-v2/components/subComponents/NoData";
+import AddRole from "./Add";
 
 const Index = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [totalPages, setTotalPages] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
+  const [action, setAction] = useState(false);
 
-	const columns = [
-		{
-			Header: '#',
-			accessor: '_id',
-			isSortable: false,
-			width: 10,
-			display: false,
-		},
+  const [addRoleModal, setAddRoleModal] = useState(false);
 
-		{ Header: 'Role Name', accessor: 'roleName' },
-		{ Header: 'Description', accessor: 'description' },
-		{ Header: 'Action', accessor: 'action' },
-	];
-	// const { isOpen, onOpen, onClose } = useDisclosure();
-	const [action, setAction] = useState(false);
-	const [isLoding, setIsLoding] = useState(false);
-	const [data, setData] = useState([]);
-	const size = 'lg';
+  const columns = ["SR.No", "Role Name", "Description", "action"];
 
-	// const handleClick = () => {
-	//   onOpen();
-	// };
+  const buildQueryParams = () => {
+    const params = {
+      page: currentPage,
+      limit: pageSize,
+    };
 
-	const fetchData = async () => {
-		setIsLoding(true);
-		let result = await getApi('api/role-access');
-		setData(result.data);
-		setIsLoding(false);
-	};
+    return params;
+  };
+  const { data, isLoading, isFetching, refetch } = useFetchItemsQuery(
+    { path: `/role-access/v2`, params: buildQueryParams() },
+    { refetchOnMountOrArgChange: true }
+  );
 
-	return (
-		<div>
-			{/* <Grid templateColumns="repeat(6, 1fr)" mb={3} gap={1}>
-                <GridItem colStart={6} textAlign={"right"}>
-                    <Button onClick={() => handleClick()} leftIcon={<AddIcon />} variant="brand">Add</Button>
-                </GridItem>
-            </Grid> */}
-			<AppButton
-				ml='2'
-				leftIcon={<IoArrowBack />}
-				onClick={() => navigate(-1)}
-				mb={4}
-			>
-				Back
-			</AppButton>
-			<CheckTable
-				// isOpen={isOpen}
-				// onClose={onClose}
-				tableData={data}
-				setAction={setAction}
-				fetchData={fetchData}
-				action={action}
-				columnsData={columns}
-				isLoding={isLoding}
-			/>
-		</div>
-	);
+  useEffect(() => {
+    if (data) {
+      setTotalPages(data.totalPages || 0);
+      setTotalItems(data.totalRecords || 0);
+    }
+  }, [data]);
+
+  const handlePageSizeChange = (newPageSize) => {
+    setPageSize(newPageSize);
+    setCurrentPage(1);
+  };
+
+  return (
+    <div>
+      <AppButton
+        ml="2"
+        leftIcon={<IoArrowBack />}
+        onClick={() => navigate(-1)}
+        mb={4}
+      >
+        Back
+      </AppButton>
+      <Box bg="white" px={2}>
+        <Flex
+          justifyContent="space-between"
+          p={3}
+          alignItems={"center"}
+          flexDir={{ base: "column", sm: "column", md: "row" }}
+        >
+          <Text fontSize="20px" fontWeight="bold" color="black" p={3}>
+            Roles (
+            <CountUpComponent
+              key={data?.totalRecords}
+              targetNumber={data?.totalRecords}
+            />
+            )
+          </Text>
+
+          <Button
+            leftIcon={<AddIcon />}
+            colorScheme="brand"
+            size="sm"
+            borderRadius="md"
+            py={3}
+            px={6}
+            onClick={() => setAddRoleModal(true)}
+          >
+            Add Role
+          </Button>
+        </Flex>
+
+        <Box my={2}>
+          <TopPagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={setCurrentPage}
+            totalItems={totalItems}
+            itemsPerPage={pageSize}
+            setPageSize={setPageSize}
+            handlePageSize={handlePageSizeChange}
+            refetching={isLoading}
+            loading={isLoading}
+          />
+        </Box>
+
+        <Box
+          borderRadius="4px"
+          boxShadow="sm"
+          borderWidth="1px"
+          overflow="hidden"
+        >
+          <Box position="relative" maxH="120vh" overflowY="auto">
+            <Table variant="striped" size="lg">
+              <Thead
+                position="sticky"
+                top={0}
+                bg="white"
+                zIndex={2}
+                boxShadow="0px 2px 8px rgba(0, 0, 0, 0.1)"
+                fontSize={"16px"}
+                borderRadius="lg"
+              >
+                <Tr>
+                  {columns.map((header, index) => (
+                    <Th
+                      key={index}
+                      bg="brand.200"
+                      whiteSpace="nowrap"
+                      py={4}
+                      textAlign="center"
+                    >
+                      <Text
+                        fontSize="14px"
+                        fontWeight="600"
+                        color="gray.700"
+                        textTransform="capitalize"
+                      >
+                        {header}
+                      </Text>
+                    </Th>
+                  ))}
+                </Tr>
+              </Thead>
+
+              {isLoading || isFetching ? (
+                <TableLoading columns={columns} length={7} py="4" />
+              ) : (
+                <Tbody>
+                  {data && data?.data?.length > 0 ? (
+                    data?.data.map((role, index) => (
+                      <Tr key={index}>
+                        <Td
+                          py={4}
+                          fontSize={{ base: "12px", md: "14px" }}
+                          fontWeight="400"
+                          minWidth="100px"
+                          textAlign={"center"}
+                        >
+                          {index + 1}
+                        </Td>
+                        <Td
+                          whiteSpace="nowrap"
+                          minWidth="200px"
+                          overflow="hidden"
+                          textOverflow="ellipsis"
+                        >
+                          {role.roleName}
+                        </Td>
+                        <Td textAlign="center">{role.description || "N/A"}</Td>
+                        <Td textAlign="center">
+                          <Button
+                            size="sm"
+                            borderRadius={"md"}
+                            colorScheme="brand"
+                            onClick={() =>
+                              navigate(
+                                `/user-permission/${role?._id}/${role.roleName}`
+                              )
+                            }
+                          >
+                            View Permission
+                          </Button>
+                        </Td>
+                      </Tr>
+                    ))
+                  ) : (
+                    <Tr borderColor="gray.200" textAlign="center">
+                      <Td
+                        borderBottom="none"
+                        colSpan="13"
+                        fontSize={{ base: "12px", md: "15px" }}
+                        fontWeight="500"
+                        color="gray.500"
+                        textAlign="center"
+                      >
+                        <NoData label="listing" />
+                      </Td>
+                    </Tr>
+                  )}
+                </Tbody>
+              )}
+            </Table>
+          </Box>
+        </Box>
+      </Box>
+      <AddRole
+        isOpen={addRoleModal}
+        size={"sm"}
+        setAction={setAction}
+        onClose={setAddRoleModal}
+        fetchData={() => refetch()}
+      />
+    </div>
+  );
 };
 
 export default Index;
