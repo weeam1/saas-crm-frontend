7c001f34b2efb4cdecb0ac6790e53acc61763892
naimatofficial <naimat.official.10@gmail.com>
2025-11-28 10:28:06 +0500

    update user register add team leader added

--- src/views/admin/users/Edit.js
+++ src/views/admin/users/Edit.js
@@ -76,7 +76,7 @@ const Edit = (props) => {
 	} = useDisclosure();
 
 	const [replacementManager, setReplacementManager] = useState('');
-	const [replacementTeamLead, setReplacementTeamLead] = useState('');
+	const [replacementTeamLead, setReplacementTeamLead] = useState(null);
 	const [securityPassword, setSecurityPassword] = useState('');
 
 	const controller = new AbortController();
@@ -112,24 +112,24 @@ const Edit = (props) => {
 
 	const { user, isSuperAdmin } = useUserSession();
 
-	const [filteredAgents, setFilteredAgents] = useState([]);
+	// const [filteredAgents, setFilteredAgents] = useState([]);
 
 	const managers = useMemo(() => tree?.data || [], [tree?.data]);
 
-	const handleManagerChange = (e) => {
-		const selectedManagerId = e.target.value;
-		handleChange(e); // Update form values
-		if (selectedManagerId) {
-			const agentsKey = `manager-${selectedManagerId}`;
-			const agentsList = tree?.agents[agentsKey] || [];
+	// const handleManagerChange = (e) => {
+	// 	const selectedManagerId = e.target.value;
+	// 	handleChange(e); // Update form values
+	// 	if (selectedManagerId) {
+	// 		const agentsKey = `manager-${selectedManagerId}`;
+	// 		const agentsList = tree?.agents[agentsKey] || [];
 
-			// filter only agents they have teamLeader is null
-			const filtered = agentsList?.filter((agent) => !agent?.teamLeader);
-			setFilteredAgents(filtered);
-		} else {
-			setFilteredAgents([]);
-		}
-	};
+	// 		// filter only agents they have teamLeader is null
+	// 		const filtered = agentsList?.filter((agent) => !agent?.teamLeader);
+	// 		setFilteredAgents(filtered);
+	// 	} else {
+	// 		setFilteredAgents([]);
+	// 	}
+	// };
 
 	const formik = useFormik({
 		initialValues: initialValues,
@@ -168,14 +168,12 @@ const Edit = (props) => {
 
 	const EditData = async () => {
 		try {
-			const role = roles.find((role) => role?._id === values.role);
+			const role = roles?.find((role) => role?._id === values.role);
 			const isAgentOrTeamLeadRole = ['Team Leader', 'Agent'].includes(
 				role?.roleName
 			);
 
 			const valuesObj = { ...values };
-
-			console.log('update user');
 
 			// when team lead role change to other role
 			if (
@@ -188,7 +186,7 @@ const Edit = (props) => {
 				return;
 			} else if (replacementTeamLead) {
 				valuesObj['replacementTeamLead'] = replacementTeamLead;
-			}
+			} else valuesObj['replacementTeamLead'] = null;
 
 			if (
 				data?.roles[0]?.roleName === 'Manager' &&
@@ -220,10 +218,9 @@ const Edit = (props) => {
 				delete valuesObj['parent'];
 			} else {
 				delete valuesObj['parent'];
-				delete valuesObj['teamLead'];
 			}
 
-			if (role?.roleName === 'Team Leader') {
+			if (role?.roleName !== 'Agent') {
 				delete valuesObj['teamLead'];
 			}
 
@@ -302,7 +299,7 @@ const Edit = (props) => {
 		} catch (e) {
 			console.log(e);
 			if (!controller.signal.aborted) {
-				toast.error(e?.data?.error || 'User is not updated!');
+				toast.error(e?.data?.message || 'User is not updated!');
 				setReplacementManager('');
 				setSecurityPassword('');
 			}
