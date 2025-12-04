848ba1c9dd27224ce43d1dce726d0819bdf65e31
Muhammad Arslan <arslanzafarr71@gmail.com>
2025-12-04 13:43:07 +0500

    feat resdesign lead preview popup with dynamic data completed

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,7 +1,7 @@
 const keys = {
-  //   nodeENV: process.env.REACT_APP_NODE_ENV,
-  //   baseLocalUrl: process.env.REACT_APP_WEEAM_LOCAL_API,
-  //   baseLiveUrl: process.env.REACT_APP_WEEAM_LIVE_API,
+  // nodeENV: process.env.REACT_APP_NODE_ENV,
+  // baseLocalUrl: process.env.REACT_APP_WEEAM_LOCAL_API,
+  // baseLiveUrl: process.env.REACT_APP_WEEAM_LIVE_API,
 
   nodeENV: "development",
   baseLocalUrl: "https://stageapi.weeam.info/",
