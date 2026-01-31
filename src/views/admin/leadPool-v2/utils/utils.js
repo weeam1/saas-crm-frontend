04517ff73aecc195e81fee410014f4b869254f16
naimatofficial <naimat.official.10@gmail.com>
2026-01-31 17:31:48 +0500

    update lead pool coins issue and optimized api  in  admin leadpool  side

--- src/views/admin/leadPool-v2/utils/utils.js
+++ src/views/admin/leadPool-v2/utils/utils.js
@@ -1,30 +1,35 @@
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const COIN_COST_BY_STATUS = {
+	new: 300,
+	default: 50,
+};
 
 export const handleCopy = (text) => {
-  navigator.clipboard
-    .writeText(text)
-    .then(() => {
-      toast.success("Copied to clipboard!", {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        theme: "light",
-      });
-    })
-    .catch((err) => {
-      toast.error("Failed to copy!", {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        theme: "light",
-      });
-      console.error("Clipboard error: ", err);
-    });
+	navigator.clipboard
+		.writeText(text)
+		.then(() => {
+			toast.success('Copied to clipboard!', {
+				position: 'top-right',
+				autoClose: 2000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				theme: 'light',
+			});
+		})
+		.catch((err) => {
+			toast.error('Failed to copy!', {
+				position: 'top-right',
+				autoClose: 2000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				theme: 'light',
+			});
+			console.error('Clipboard error: ', err);
+		});
 };
